43e633f0fc8f0a086ca8cf59567682383de7a437
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2024-01-06 09:18:49 -0600

    Added LINQ extension methods and unit tests

--- CSharpExtender/ExtensionMethods/LinqExtensionMethods.cs
+++ CSharpExtender/ExtensionMethods/LinqExtensionMethods.cs
@@ -4,25 +4,62 @@ namespace CSharpExtender.ExtensionMethods;
 
 public static class LinqExtensionMethods
 {
+    /// <summary>
+    /// Checks if none of the elements in the collection satisfy the provided condition.
+    /// If no condition is provided, it checks if the collection is empty.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    /// <param name="elements">The collection to check.</param>
+    /// <param name="func">The condition to check each element against.</param>
+    /// <returns>True if none of the elements satisfy the condition, false otherwise.</returns>
     public static bool None<T>(this IEnumerable<T> elements, Func<T, bool>? func = null)
     {
-        return func == null
-            ? !elements.Any()
-            : !elements.Any(func.Invoke);
+        if(func == null)
+        {
+            return !elements.Any();
+        }
+
+        return !elements.Any(func);
     }
 
-    public static void ApplyToEach<T>(this IEnumerable<T> elements, Action<T> func)
+    /// <summary>
+    /// Applies an action to each element in the collection.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    /// <param name="elements">The collection to iterate over.</param>
+    /// <param name="action">The action to apply to each element.</param>
+    public static void ForEach<T>(this IEnumerable<T> elements, Action<T> action)
     {
         foreach (T element in elements)
         {
-            func(element);
+            action(element);
         }
     }
 
+    /// <summary>
+    /// Applies an action to each element in the list.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the list.</typeparam>
+    /// <param name="list">The list to iterate over.</param>
+    /// <param name="action">The action to apply to each element.</param>
+    public static void ForEach<T>(this List<T> list, Action<T> action)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            action(list[i]);
+        }
+    }
+
+    /// <summary>
+    /// Returns a random element from the list.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the list.</typeparam>
+    /// <param name="options">The list to pick a random element from.</param>
+    /// <returns>A random element from the list, or default(T) if the list is empty.</returns>
     public static T? RandomElement<T>(this List<T> options)
     {
-        return options.None() 
-            ? default 
+        return options.Count == 0
+            ? default
             : options[RngCreator.GetNumberBetween(0, options.Count - 1)];
     }
 }
