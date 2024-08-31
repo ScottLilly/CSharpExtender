583a0a22e32c368382a2b9fcd5cbc25978c7ac21
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2024-08-31 09:38:50 -0400

    Added ObservableModel and PropertyChangeTrackingModel classes, for simpler property changed notifications and tracking changes

---
+++ CSharpExtender/Models/ObservableModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CSharpExtender.Models
+{
+    public abstract class ObservableModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual bool SetProperty<T>(ref T backingField, T value, 
+            [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(backingField, value))
+            {
+                return false;
+            }
+
+            backingField = value;
+
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ CSharpExtender/Models/PropertyChangeTrackingModel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CSharpExtender.Models
+{
+    /// <summary>
+    /// Base model, used for property changed notification and tracking changed property values.
+    /// </summary>
+    public class PropertyChangeTrackingModel : ObservableModel, IChangeTracking
+    {
+        private bool _isChanged;
+
+        public bool IsChanged
+        {
+            get => _isChanged;
+            private set
+            {
+                SetProperty(ref _isChanged, value);
+            }
+        }
+
+        public ObservableCollection<PropertyChangedLog> PropertyChangeLog { get; } = 
+            new ObservableCollection<PropertyChangedLog>();
+
+        public PropertyChangeTrackingModel()
+        {
+            PropertyChangeLog.CollectionChanged += (s, e) =>
+            {
+                IsChanged = PropertyChangeLog.Count > 0;
+            };
+        }
+
+        protected override bool SetProperty<T>(ref T field, T value, 
+            [CallerMemberName] string propertyName = null)
+        {
+            bool propertyChanged = base.SetProperty(ref field, value, propertyName);
+
+            if (propertyChanged)
+            {
+                PropertyChangeLog.Add(new PropertyChangedLog(propertyName, value));
+            }
+
+            return propertyChanged;
+        }
+
+        public void AcceptChanges()
+        {
+            PropertyChangeLog.Clear();
+        }
+    }
+}
